19585b6c07169962648b42ebdea305eacc191d38
Vibhav Deepak <70058308+VibeyVib@users.noreply.github.com>
2021-11-11 09:33:16 +0530

    about page updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,18 @@
 import react from "react";
-
+import '../components/About.css';
+import mascot from '../assets/mascotello.png';
 function About(){
     return(
-        <div className="Hi">
-            <h1>Hello About</h1>
-        </div>
+        <div className="container">
+            <div className="img">
+                <img className="elloimg" src={mascot} alt="ello"/>
+            </div>
+            <div className="herocont">
+                <h1>This is Ellobot.<br></br>
+                    Your Virtual Companion.
+                </h1>
+            </div>
+            </div>
     );
 }
 
